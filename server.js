42e1b1f870a5626f85e6ee1f15bf21fef158489a
ecuyle <ecuyle@gmail.com>
2019-03-19 08:33:21 -0700

    Update server to expose new ghosttrain endpoint

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const StationsRouter = require('./Routers/Stations.js');
 const PredictionsRouter = require('./Routers/Predictions.js');
 const InitDelayRouter = require('./Routers/InitDelay.js');
+const GhostRouter = require('./Routers/Ghost.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,6 +16,7 @@ app.use(cors());
 app.use('/v1/api/stations', StationsRouter);
 app.use('/v1/api/predictions', PredictionsRouter);
 app.use('/v1/api/initDelay', InitDelayRouter);
+app.use('/v1/api/ghosttrain', GhostRouter);
 
 app.listen(PORT, err => {
   if (err) throw err;
